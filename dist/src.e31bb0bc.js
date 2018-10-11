74840bd9baafbb82f4f3b321a57281062d07db61
Ahmad Ibrahim <ibrahim@revenuemonster.my>
2018-10-11 12:57:09 +0800

    add dist and cache file in git ingnore

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -45303,7 +45303,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57857" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56394" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
