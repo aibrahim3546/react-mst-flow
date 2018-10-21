d4a391b18c1e7a1651aee814f8d0cca7ccbbedc5
ibrahim <aibrahim3546@gmail.com>
2018-10-21 20:39:36 +0800

    remove config file

--- src/config.js
+++
@@ -1,5 +0,0 @@
-// Note that API KEY are normally stored in the backend.
-// This app was made for fun and learning purpose only only.
-const API_KEY = '467e793b630900b9b24ff78059de0b01';
-
-export { API_KEY };
